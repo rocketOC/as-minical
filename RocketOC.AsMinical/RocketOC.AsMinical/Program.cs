3768fc2328c3b2ba88f2f419406f54c2282197ad
rocketOC <11656159+rocketOC@users.noreply.github.com>
2022-08-22 20:39:03 -0400

    wrapping support

--- RocketOC.AsMinical/RocketOC.AsMinical/Program.cs
+++ RocketOC.AsMinical/RocketOC.AsMinical/Program.cs
@@ -1,9 +1,11 @@
 ﻿using RocketOC.MinicalDotnet;
+using System.CommandLine;
 
-//only accept piped-input for now
+//only accept piped input for now
 if (!Console.IsInputRedirected)
     return 0;
 
+//parse the input data
 //may change to list in future so we can use frequency info for something
 var activity = new HashSet<DateOnly>();
 while (Console.In.Peek() != -1)
@@ -17,9 +19,30 @@ while (Console.In.Peek() != -1)
     }
 }
 
-Console.WriteLine();
+//setup parsing for the command line arguments
+var rootCommand = new RootCommand("Make a mini calendar");
 
-var mini = new Minical();
-mini.PrintActivity(activity, 2, true);
+var wrap = new Option<int>
+    (name: "--wrap",
+    description: "Wrap to a new row after X months",
+    getDefaultValue: () => -1);
 
+var sep = new Option<int>
+    (name: "--sep",
+    description: "Separate the months by X blocks. O and 2 are nice",
+    getDefaultValue: () => 0);
+
+rootCommand.AddOption(wrap);
+rootCommand.AddOption(sep);
+
+rootCommand.SetHandler((wrapOptionValue, separtationOptionValue) =>
+{
+    Console.WriteLine();
+    var mini = new Minical();
+    mini.PrintActivity(activity, separtationOptionValue, true, wrapOptionValue);
+}, wrap, sep);
+
+
+//process the commands
+await rootCommand.InvokeAsync(args);
 return 0;
